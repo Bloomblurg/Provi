2c7b29c88671330cf9367364a2d8a3a597faac66
Johannes Bloomblurg <Bloomblurg@protonmail.com>
2021-10-16 08:55:19 -0700

    prep

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ const path = require("path");
 
 //GitHub Pages requires dist files to be in a /docs/ folder or root
 module.exports = {
-  outputDir: path.resolve(__dirname, "./docs/")
+  outputDir: path.resolve(__dirname, "./docs/"),
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Provi/'
+    : '/'
 }
