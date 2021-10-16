b94b368e84026e93dc020dc828e7e4ca2723987a
Johannes Bloomblurg <Bloomblurg@protonmail.com>
2021-10-16 08:36:56 -0700

    prep

--- src/main.js
+++ src/main.js
@@ -3,16 +3,19 @@ import App from './App.vue'
 import router from './router'
 //import store from './store'
 import PrimeVue from 'primevue/config';
-//import Axios from './axios'
+import Axios from './axios'
 
 import 'primeflex/primeflex.css';
 import './style/root.css';
 import './style/themeDark.css';
 
 
-createApp(App)
-	//.provide('$axios', Axios)  //Not installed, might not be needed
+
+let app = createApp(App)
 	//.use(store)
 	.use(router)
 	.use(PrimeVue)
-	.mount('#app')
+
+app.config.globalProperties.$axios = Axios
+
+app.mount('#app')
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -3,6 +3,8 @@
 <div class="p-grid grid">
 	<div class="p-col">
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec quam lectus. Aenean ullamcorper dolor nec lorem aliquet porta. Ut massa purus, semper vel eleifend ut, rhoncus et enim. Donec libero metus, sollicitudin id risus non, ultricies maximus tortor. Sed ut fringilla risus, non feugiat enim. Nullam non eros sollicitudin, fermentum justo vitae, pulvinar urna. Donec facilisis quis purus eu ullamcorper. Nullam condimentum nisi vel dolor malesuada bibendum. Ut id nunc blandit mauris elementum malesuada. Aliquam eleifend malesuada mi, et hendrerit massa porttitor id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis vitae sapien metus. Aliquam vulputate laoreet pellentesque.</p>
+    
+    <p>{{ $axios.defaults.baseURL }}</p>
 	</div>
 </div>
 
